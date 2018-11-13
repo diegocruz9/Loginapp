40707c30d8818ec9b2d3e517f46aa96efb87dbb9
Diego Alejandro Cruz Guzman <dcruz@belatrixsf.com>
2018-11-13 15:43:13 -0500

    New activities, sign up and main menu

--- app/src/main/java/com/example/dcruz/loginapp/MainActivity.java
+++ app/src/main/java/com/example/dcruz/loginapp/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.dcruz.loginapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText name;
+    private EditText password;
+    private Button login;
+    private Button registe;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = (EditText)findViewById(R.id.etname);
+        password = (EditText)findViewById(R.id.etpass);
+        login = (Button)findViewById(R.id.btnlogin);
+        registe =(Button)findViewById(R.id.btnregis);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(name.getText().toString(), password.getText().toString());
+            }
+        });
+
+        registe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, register.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void validate(String userName, String userPassword)
+    {
+        if ((userName.equals("admin")) && (userPassword.equals("1234")))
+        {
+            Intent intent = new Intent(MainActivity.this, MainMenu.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/dcruz/loginapp/register.java
+++ app/src/main/java/com/example/dcruz/loginapp/register.java
@@ -1,13 +1,28 @@
 package com.example.dcruz.loginapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class register extends AppCompatActivity {
+
+    private Button sign;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        sign = (Button)findViewById(R.id.btnsign);
+
+        sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(register.this, MainMenu.class);
+                startActivity(intent);
+            }
+        });
     }
 }
